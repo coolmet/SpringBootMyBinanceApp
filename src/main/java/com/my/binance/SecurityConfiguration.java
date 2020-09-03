68b9ae64b00e7ecd12c4d8c45bac90bbb36162e1
Metehan <metehanmeral@hotmail.com>
2020-09-03 21:52:10 +0300

    20200903_2

--- src/main/java/com/my/binance/SecurityConfiguration.java
+++ src/main/java/com/my/binance/SecurityConfiguration.java
@@ -98,7 +98,7 @@ public class SecurityConfiguration
 			    .antMatchers(HttpMethod.POST,"/restlogin").permitAll()
 			    .antMatchers(// @formatter:off
 			                 "/rest",
-			                 "/rest/*",
+			                 "/rest/*", 
 			                 "/rest/**"
 			                 // @formatter:on
 				)
